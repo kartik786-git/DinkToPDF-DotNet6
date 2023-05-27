4d1a2f9914a6b6c8c725a21d44ba22e7794d9402
KKP <KKP@DESKTOP-2RHKJ3S>
2023-05-27 17:55:03 +0530

    Add project files.

---
+++ WebApplicationDinkToPDF/Controllers/PDFController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationDinkToPDF.Services;
+
+namespace WebApplicationDinkToPDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDFController : ControllerBase
+    {
+        private readonly PdfGenerator _pdfGenerator;
+
+        public PDFController(PdfGenerator pdfGenerator)
+        {
+            this._pdfGenerator = pdfGenerator;
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePdf()
+        {
+            string htmlContent = "<html><body><h1>Hello, World!</h1>" +
+                "<img src='https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg' " +
+                "alt='Example Image'></body></html>";
+
+
+            byte[] pdfBytes = _pdfGenerator.GeneratorPdf(htmlContent);
+
+            return File(pdfBytes, "application/pdf", "generated.pdf");
+        }
+    }
+
+}
---
+++ WebApplicationDinkToPDF/Services/PdfGenerator.cs
@@ -0,0 +1,46 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+
+namespace WebApplicationDinkToPDF.Services
+{
+    public class PdfGenerator
+    {
+        private readonly IConverter _converter;
+
+        public PdfGenerator(IConverter converter)
+        {
+            this._converter = converter;
+        }
+
+
+        public byte[] GeneratorPdf(string htmlContent) 
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10, Bottom = 10, Left = 10, Right = 10 },
+                DocumentTitle = "Generated PDF"
+            };
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                //HtmlContent = htmlContent,
+                Page = "https://www.google.com/",
+                WebSettings = { DefaultEncoding = "utf-8" },
+                HeaderSettings = { FontSize = 12, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 },
+                FooterSettings = { FontSize = 12, Line = true, Right = "© " + DateTime.Now.Year }
+            };
+
+            var document = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+           return _converter.Convert(document);
+        }
+    }
+}
